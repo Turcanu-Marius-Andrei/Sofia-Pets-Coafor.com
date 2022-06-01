f586546b95c3f8ac143206bf5e956b7a105bb78c
Turcanu-Marius-Andrei <106661866+Turcanu-Marius-Andrei@users.noreply.github.com>
2022-06-01 02:19:24 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+let myImage = document.getElementById("certificat");
+function makeBig() { 
+    myImage.width = 800; 
+    myImage.height = 600;
+} 
+
+
+function makeNormal() { 
+    myImage.width = 300;
+    myImage.height = 200; 
+} 
